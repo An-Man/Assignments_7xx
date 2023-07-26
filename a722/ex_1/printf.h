ce10b306278616ca4fc1cb804c3c63ed0d22dc39
An-Man <anna.pikkarainen@gmail.com>
2023-07-26 15:59:54 +0300

    a722 ex 1 finished

--- a722/ex_1/printf.h
+++ a722/ex_1/printf.h
@@ -3,31 +3,37 @@
 
 #include <string>
 #include <iostream>
-#include <sstream>
+#include <type_traits>
+#include <stdexcept>
 
 template <typename T, typename... Ts>
-void safe_printf(const std::string& fmt, T first, Ts... args)
+void safe_printf(std::string& fmt, T first, Ts... args)
 {
-    std::stringstream ss(fmt);
+    int pos_fs = fmt.find("%");                     // find first %
+    
+    std::string start = fmt.substr (0, pos_fs);     // make substring from start to %
+    std::cout << start;                             // print from start to %
 
-    std::string word;
+    std::string fmts = fmt.substr (pos_fs, 2);      // make substring of "%_"
+
+        if (fmts == "%s" && std::is_same_v<T, std::string>) { // check % and type of first arg
+            std::cout << first;
+        } else if (fmts == "%d" && std::is_arithmetic_v<T>) {
+            std::cout << first;
+        } else {
+            throw std::runtime_error("Bad argument type");
+        }
+    
+    std::string rest = fmt.substr (pos_fs + 2);     // make a substring of the rest of string
     
     if constexpr (sizeof...(args) > 0) {
-
-        while (ss >> word) {
-            if (word[0] == '%') {
-                word = first;
-                std::cout << word << " ";
-                safe_printf(fmt, args...);
-            } else {
-                std::cout << word << " ";
-            }
-        }
+        fmt = rest;                                 // replace string with rest
+        safe_printf(fmt, args...);                  // recur with rest of string and args
     }
 
-    std::cout << word << " ";
+    if constexpr (sizeof...(args) == 0) {
+        std::cout << rest << '\n';                  // if args are exhausted, 
+    }                                               // print rest of the string
 }
-
-
 
 #endif
